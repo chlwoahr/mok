2d248b6f260732a2895ef2286e8f5f9a28ad1077
12 <12@192.168.25.49>
2022-03-15 22:04:30 +0900

    6

--- HellowWorld/src/com/dev/ForExample5.java
+++ HellowWorld/src/com/dev/ForExample5.java
@@ -1,9 +1,32 @@
 package com.dev;
 
+import java.util.Scanner;
+
 public class ForExample5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1,num2,a = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+			System.out.println("첫번째 수");
+			num1 = sc.nextInt();
+			System.out.println("두번째 수");
+			num2 = sc.nextInt();
+			
+			if(num1>=num2) {
+				num1 -= num2;
+				System.out.println(num1);
+			}
+			else if (num1<=num2) {
+				num2 -= num1;
+				System.out.println(num2);
+			}
+			
+			
+			
+			
+		
 		
 	}
 
--- HellowWorld/src/com/edu/TodayExample.java
+++ HellowWorld/src/com/edu/TodayExample.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 
-// 이름을 입력받도록...(String)
-// 연락처 입력...(String)
-// 나이 입력...(int)
-// 키...(double)
+// �씠由꾩쓣 �엯�젰諛쏅룄濡�...(String)
+// �뿰�씫泥� �엯�젰...(String)
+// �굹�씠 �엯�젰...(int)
+// �궎...(double)
 
-// 이름은 :
-// 연락처 :
-// 나이 :
-// 키 :
-// 몸무게 :
-// 적정몸무게:(키 - 100) * 0.9 크면 과체중 작으면 저체중
+// �씠由꾩� :
+// �뿰�씫泥� :
+// �굹�씠 :
+// �궎 :
+// 紐몃Т寃� :
+// �쟻�젙紐몃Т寃�:(�궎 - 100) * 0.9 �겕硫� 怨쇱껜以� �옉�쑝硫� ��泥댁쨷
 public class TodayExample {
 
 	public static void main(String[] args) {
@@ -25,26 +25,26 @@ public class TodayExample {
 		
 		System.out.println("이름은...");
 		n = sc.nextLine();
-		System.out.println("연락처 입력...");
+		System.out.println("번호는...");
 		p = sc.nextLine();
-		System.out.println("나이 입력...");
+		System.out.println("나이는...");
 		a = sc.nextInt();
-		System.out.println("키 입력...");
+		System.out.println("�궎 �엯�젰...");
 		k = sc.nextDouble();
-		System.out.println("몸무게...");
+		System.out.println("紐몃Т寃�...");
 		m = sc.nextDouble();
-		System.out.println("이름은:"+n);
-		System.out.println("연락처:"+p);
-		System.out.println("나이:"+a);
-		System.out.println("키:"+k+"cm");
-		System.out.println("몸무게:"+m);
+		System.out.println("�씠由꾩�:"+n);
+		System.out.println("�뿰�씫泥�:"+p);
+		System.out.println("�굹�씠:"+a);
+		System.out.println("�궎:"+k+"cm");
+		System.out.println("紐몃Т寃�:"+m);
 		jj = ((k-100)*0.9) ;
 				
 		if(jj+5>m&&jj-5<m) {
-			System.out.println("정상");
+			System.out.println("�젙�긽");
 		}
 		else {
-			System.out.println("비정상");
+			System.out.println("鍮꾩젙�긽");
 			
 		}
 		}
